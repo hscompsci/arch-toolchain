1f1bd3561eff9313c77a6eb00c6c3c1ea978171c
Sol Boucher <sboucher@jisedu.or.id>
2025-03-11 19:47:19 +0700

    libc.s: Add fgets rich input support

--- libc.s
+++ libc.s
@@ -2,16 +2,33 @@
 exit:
 	.word 0
 
+	.globl fgets
+fgets:
+	mv		t0, a0
+	add		t1, a0, a1
+	li		t2, '\n'
+	mv		t3, a0
+.Lfgets_loop:
+	beq		t0, t1, .Lfgets_end
+	ebreak
+	sb		a0, (t0)
+	addi	t0, t0, 1
+	bne		a0, t2, .Lfgets_loop
+.Lfgets_end:
+	sb		zero, (t0)
+	mv		a0, t3
+	ret
+
 	.globl fputs
 fputs:
 	mv		t0, a0
-	j		.Lcondition
-.Lloop:
+	j		.Lfputs_condition
+.Lfputs_loop:
 	ecall
 	addi	t0, t0, 1
-.Lcondition:
+.Lfputs_condition:
 	lb		a0, (t0)
-	bne		a0, zero, .Lloop
+	bne		a0, zero, .Lfputs_loop
 	ret
 
 	.globl getchar
